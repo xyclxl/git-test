72f865ce8f360571bf4b78ed20598b89934fd249
lxl <1976355584@qq.com>
2023-05-26 16:56:35 +0800

    second commit

--- src/main/java/com/lxl/git/GitTest.java
+++ src/main/java/com/lxl/git/GitTest.java
@@ -7,5 +7,7 @@ package com.lxl.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
+
     }
 }
