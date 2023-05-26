c4ecfc2a4d15463a5f025b78d0a4352accf211e4
lxl <1976355584@qq.com>
2023-05-26 16:52:50 +0800

    first commit

---
+++ src/main/java/com/lxl/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lxl.git;
+
+/**
+ * @author lxl
+ * @date 2023/5/26 16:46
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
